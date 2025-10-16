f051c9a41cc14a57e5fa2c9885dd219f90c8a1c8
sahadul <sahadul80@gmail.com>
2025-10-16 16:43:19 +0600

    home page updated2

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
       <div className="border-t border-gray-800">
         <div className="container mx-auto px-4 py-8">
           <div className="text-center text-gray-400">
-            <p>&copy; 2025 Paramount Textile. All rights reserved.</p>
+            <p>2025 Paramount Textile. All rights reserved.</p>
           </div>
         </div>
       </div>
